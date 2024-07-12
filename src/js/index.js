602b14cefe38b594eecf0c5c0e01adb6118c8fdc
bertomeu <josecarlosbertomeu@gmail.com>
2024-07-12 18:58:39 +0000

    Modified index.js

--- src/js/index.js
+++ src/js/index.js
@@ -6,8 +6,22 @@ import ReactDOM from "react-dom/client";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import Home from "./component/Home.js";
 
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
 
+let counters = 0;
+
+setInterval(function () {
+    const six = Math.floor(counters/100000);
+    const five = Math.floor(counters/10000);
+    const four = Math.floor(counters/1000);
+    const three = Math.floor(counters/100);
+    const two = Math.floor(counters/10);
+    const one = Math.floor(counters/1);
+    counters++;
+
+    ReactDOM.createRoot(document.getElementById("app")).render(<Home numberOne={one} numberTwo={two} numberThree={three} numberFour={four} numberFive={five} numberSix={six}/>);
+    
+    
+}, 1000);
